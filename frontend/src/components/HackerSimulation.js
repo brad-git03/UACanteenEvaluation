87848c608077e1b608c7f93c35fb2b3397d30c3f
brad-git03 <bsmanalese.student@ua.edu.ph>
2026-04-08 23:03:44 +0800

    fix warning in hackersimulation.js

--- frontend/src/components/HackerSimulation.js
+++ frontend/src/components/HackerSimulation.js
@@ -19,6 +19,7 @@ export default function HackerSimulation({ navigate }) {
     if (isAuthenticated) {
       fetchData();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated]);
 
   const fetchData = async () => {
