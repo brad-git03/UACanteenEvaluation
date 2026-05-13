a1125c87f43f034f3245e893291881f41338d993
brad-git03 <bsmanalese.student@ua.edu.ph>
2026-05-13 12:03:28 +0800

    Added server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('dns').setDefaultResultOrder('ipv4first'); // <--- ADD THIS CRITICAL LINE
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
